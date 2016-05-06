4309e31974954998c1013d5a54f507cd387459ea
agconti <agc11d@gmail.com>
2016-05-06 17:12:45 -0400

    refactor(unlitMaterial): moved shaders to seperate files

--- unlitMaterial.ts
+++ unlitMaterial.ts
@@ -1,27 +1,5 @@
 import * as THREE from 'three'
-
-const vertexShader =
-`
-/**
- * Multiply each vertex by the model-view matrix
- * and the projection matrix (both provided by
- * Three.js) to get a final vertex position
- */
-void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
-`
-const fragmentShader =
-`
-/**
- * Set the colour to a lovely pink.
- * Note that the color is a 4D Float
- * Vector, R,G,B and A and each part
- * runs from 0.0 to 1.0
- */
-void main() {
-  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-}
-`
+import {vertexShader} from './simpleVertexShader'
+import {fragmentShader} from './solidColorPinkFragmentShader'
 
 export const unlitMaterial = new THREE.ShaderMaterial({vertexShader, fragmentShader})
